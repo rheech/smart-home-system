56ce08f1dd9c5026c0bc87a06bae25964835706b
rheech <rheech@9e240af0-2095-8f48-abc0-f1d36e134acd>
2015-11-02 08:40:04 +0000

    Fix autoscale mode

--- vs2010/libdeviceui/frmDeviceUI.Designer.cs
+++ vs2010/libdeviceui/frmDeviceUI.Designer.cs
@@ -64,7 +64,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(440, 220);
+            this.tabPage2.Size = new System.Drawing.Size(376, 240);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Log";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -77,13 +77,12 @@
             this.txtLogConsole.Name = "txtLogConsole";
             this.txtLogConsole.ReadOnly = true;
             this.txtLogConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLogConsole.Size = new System.Drawing.Size(434, 214);
+            this.txtLogConsole.Size = new System.Drawing.Size(370, 234);
             this.txtLogConsole.TabIndex = 0;
             // 
             // frmDeviceUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(384, 312);
             this.Controls.Add(this.tabMain);
             this.Name = "frmDeviceUI";
